421d109599fc8bd2b8fc8ddc4ddfe2c452596916
zhenghaoyu <zhenghaoyuzhy@163.com>
2023-12-03 12:49:57 +0800

    Site updated: 2023-12-03 12:49:55

--- js/bibi.js
+++ js/bibi.js
@@ -3,7 +3,7 @@ let total = 0
 let nowNum = 0
 let items = []
 let page = 1
-let Url = 'https://kkapi.fomal.cc/api/ispeak?author=6319fedef46fae97dcfa5ee2&page=' // 记住替换为你的API链接
+let Url = '' // 记住替换为你的API链接
 
 
 window.addEventListener('DOMContentLoaded', () => {
